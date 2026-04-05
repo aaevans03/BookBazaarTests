2fda526ec8c44f040965335c5b3c11e30c96bad5
aaevans03 <aaevans03@gmail.com>
2026-04-04 20:23:37 -0600

    [test] Happy path test, GET requests

--- bookbazaar.test.js
+++ bookbazaar.test.js
@@ -7,6 +7,7 @@ test("Happy path test", async () => {
         author: "J. R. R. Tolkien",
     }
     
+    // POST: Add new book
     const postRes = await fetch(`${apiUrl}/api/books`, {
         method: "POST",
         headers: { 'Content-Type': 'application/json', },
@@ -21,8 +22,44 @@ test("Happy path test", async () => {
     expect(postResJson.author).toBe(newBook.author);
     expect(postResJson.id).toEqual(expect.any(String));
 
-    const bookId = Number(postResJson.id)
-
+    // Check ID of the book we added
+    const bookId = Number(postResJson.id);
     expect(bookId).toEqual(expect.any(Number));
+
     
+    // GET: Get list of books, expect the book we just created to be inside
+    const getRes = await fetch(`${apiUrl}/api/books`);
+    
+    expect(getRes.status).toBe(200);
+    
+    const getResJson = await getRes.json();
+
+    expect(Array.isArray(getResJson)).toBe(true);
+
+    // Check the format of the response
+    for (const value of getResJson) {
+        expect(value).toEqual(
+            expect.objectContaining({
+                title: expect.any(String),
+                author: expect.any(String),
+                id: expect.any(String),
+            })
+        );
+    }
+
+    // Check to see if response has the book we just added
+    expect(getResJson).toEqual(
+        expect.arrayContaining([postResJson])
+    );
+
+    
+    // GET: Get book by id
+    const getIdRes = await fetch(`${apiUrl}/api/books/${bookId}`);
+
+    expect(getIdRes.status).toBe(200);
+
+    const getIdResJson = await getIdRes.json();
+    expect(getIdResJson).toEqual(
+        expect.objectContaining(postResJson)
+    );    
 });
